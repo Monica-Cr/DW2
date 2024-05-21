eba4b38e1896a1ee43ab05a02ce619981d1ab697
Monica-Cr <133968574+Monica-Cr@users.noreply.github.com>
2024-05-21 21:51:26 +0200

    trial 2

--- build-en-start-op-poort-81.bat
+++ build-en-start-op-poort-81.bat
@@ -1,3 +1,3 @@
 docker image build -t image-opdracht .
-docker container run -dt --name container-opdracht image-opdracht 
+docker container run -d -t -p 80:81--name container-opdracht image-opdracht 
 
